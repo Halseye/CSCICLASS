b942ce81c9266413d26f72760499647f3c242367
Gregory J-ctr Walsh <gregory.j-ctr.walsh@faa.gov>
2021-09-30 19:59:40 -0400

    week3

--- Week3/BankAccount.java
+++ Week3/BankAccount.java
@@ -1,5 +1,7 @@
 package Week3;
 
+import java.util.Scanner;
+
 public class BankAccount {
     private double balance;
 
@@ -36,5 +38,22 @@ public class BankAccount {
         System.out.println("Amount after "+ t + " years: "+ amount);
     }
 
+    public static int menu() {
+        
+        int selection;
+        Scanner input = new Scanner(System.in);
 
+        /*******************************************/
+        System.out.println("Please select an option:");
+        System.out.println("------------------------\n");
+        System.out.println("1: Get Balance");
+        System.out.println("2: Deposit");
+        System.out.println("3: Withdraw");
+        System.out.println("0: Quit");
+        System.out.println("------------------------\n");
+
+        selection = input.nextInt();
+
+        return selection;
+    }
 }
